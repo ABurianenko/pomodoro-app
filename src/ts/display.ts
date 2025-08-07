8780666664e57b2131acbe6a6e8058fa696a4a88
ABurianenko <121970095+ABurianenko@users.noreply.github.com>
2025-08-07 16:13:27 +0100

    start timer

--- src/ts/display.ts
+++ src/ts/display.ts
@@ -1,20 +1,13 @@
 import { timerState } from "./state";
 
-//перенести в switchMode()
-const mode = timerState.mode;
-const duration = timerState.durations[mode];
-let timeLeft = timerState.timeLeft;
-timeLeft = duration * 60;
 
 const display = document.querySelector('.timer');
 
+export function updateDisplay () {
+    let minutes = Math.floor(timerState.timeLeft / 60);
+    let seconds = timerState.timeLeft % 60;
 
-
-function updateDisplay () {
-    let minutes = Math.floor(timeLeft / 60);
-    let seconds = timeLeft % 60;
-
-    display?.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+    return display.textContent = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
 }
 
-export {}
+display?.addEventListener('change', updateDisplay);
---
+++ src/ts/timer.ts
@@ -0,0 +1,43 @@
+import { updateDisplay } from "./display";
+import { timerState } from "./state";
+
+//перенести в switchMode()
+// const mode = timerState.mode;
+// const duration = timerState.durations[mode];
+// let timeLeft = timerState.timeLeft;
+// timeLeft = duration * 60;
+
+const startBtn = document.querySelector('.start-pause')
+const resetBtn = document.getElementById('reset');
+const btnIcon = document.getElementById('btn');
+
+const startTimer = () => {
+    if (timerState.isRunning) {
+        return
+    }
+
+    timerState.isRunning = true;
+    btnIcon?.setAttribute("href", "./src/svg/symbol-defs.svg#icon-pause");
+
+    timerState.intervalId = setInterval(() => {
+        
+        timerState.timeLeft--;
+
+        if (timerState.timeLeft === 0) {
+            clearInterval(timerState.intervalId);
+            timerState.isRunning = false;
+            btnIcon?.setAttribute("href", "./src/svg/symbol-defs.svg#icon-Play");
+        }
+
+        updateDisplay();
+    }, 1000)
+}
+
+const resetTimer = () => {
+    
+}
+
+resetBtn?.addEventListener('click', resetTimer);
+startBtn?.addEventListener('click', startTimer)
+
+export { };
